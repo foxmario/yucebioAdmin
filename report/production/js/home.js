1ff7d17770b87502aec13bc5f1a1dfa6c38487ba
liuyiyang <liuyiyang@yucebio.com>
2018-06-28 17:14:35 +0800

    修改跳转

--- report/production/js/home.js
+++ report/production/js/home.js
@@ -1,8 +1,6 @@
-(function(angular){
-	var app = angular.module('myApp',['smart-table','ui.bootstrap', 'ngRoute','tool']);
-	app.controller('safeCtrl',['$scope','$routeParams','$http','$location',function($scope,$routeParams,$http,$location){
-
-	
+(function(angular) {
+    var app = angular.module('myApp', ['smart-table', 'ui.bootstrap', 'ngRoute', 'tool']);
+    app.controller('safeCtrl', ['$scope', '$routeParams', '$http', '$location', function($scope, $routeParams, $http, $location) {
 
 
 
@@ -45,5 +43,38 @@
 
 
 
-	}])
+
+
+    }])
+
+    app.factory("httpInterceptor", ["$q", "$rootScope", function($q, $rootScope) {
+        return {
+            request: function(config) {
+                // 请求成功
+                var USER = sessionStorage.getItem('user');
+                if (!USER) {
+                    window.location.href = 'login';
+                }
+                return config || $q.when(config);
+            },
+            　　requestError: function(rejection) {　　　　 // 请求失败
+                　　　　
+                return $q.reject(rejection)　　
+            },
+            response: function(response) {
+                // do something on response success
+                return response || $q.when(response);
+            },
+            responseError: function(rejection) {
+                // do something on response error
+                return $q.reject(rejection);
+            }
+        };
+    }]);
+
+    app.config(["$httpProvider", function($httpProvider) {　　
+        $httpProvider.interceptors.push("httpInterceptor");
+    }]);
+
+
 })(angular);
--- report/production/js/project.js
+++ report/production/js/project.js
@@ -383,7 +383,34 @@
     })
 
 
+    app.factory("httpInterceptor", ["$q", "$rootScope", function($q, $rootScope) {
+        return {
+            request: function(config) {
+                // 请求成功
+                var USER = sessionStorage.getItem('user');
+                if (!USER) {
+                    window.location.href = 'login';
+                }
+                return config || $q.when(config);
+            },
+            　　requestError: function(rejection) {　　　　 // 请求失败
+                　　　　
+                return $q.reject(rejection)　　
+            },
+            response: function(response) {
+                // do something on response success
+                return response || $q.when(response);
+            },
+            responseError: function(rejection) {
+                // do something on response error
+                return $q.reject(rejection);
+            }
+        };
+    }]);
 
+    app.config(["$httpProvider", function($httpProvider) {　　
+        $httpProvider.interceptors.push("httpInterceptor");
+    }]);
 
 
 
